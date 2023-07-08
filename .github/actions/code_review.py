a4679e4e1b9e498b95c6a4fc6590ac7954c51346
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-08 13:55:45 +0200

    Update code_review.py

--- .github/actions/code_review.py
+++ .github/actions/code_review.py
@@ -18,7 +18,9 @@ def get_changed_files(pr):
     repo = git.Repo.clone_from(pr.base.repo.clone_url, to_path='./repo', branch=pr.head.ref)
 
     # Get the difference between the PR branch and the base branch
-    diffs = repo.git.diff(f"{pr.base.repo.full_name}/{pr.base.ref}", f"{pr.head.repo.full_name}/{pr.head.ref}", name_only=True).split('\n')
+    base_ref = f"origin/{pr.base.ref}"
+    head_ref = f"origin/{pr.head.ref}"
+    diffs = repo.git.diff(base_ref, head_ref, name_only=True).split('\n')
 
     # Initialize an empty dictionary to store file contents
     files = {}
