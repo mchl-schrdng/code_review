5ad80f32b1843c8569ed462951faae86eae0ba19
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-10 08:40:55 +0200

    Update code_review.py

--- .github/actions/code_review.py
+++ .github/actions/code_review.py
@@ -73,7 +73,7 @@ def send_to_openai(files):
     for chunk in chunks:
         # Send a message to OpenAI with each chunk of the code for review
         message = openai.ChatCompletion.create(
-            model="text-davinci-003",
+            model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "user",
