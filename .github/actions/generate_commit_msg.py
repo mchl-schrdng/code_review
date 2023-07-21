c5a0b703cc2dea88b6091c868b5e373b2e9f0230
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-21 09:18:09 +0200

    Update generate_commit_msg.py

--- .github/actions/generate_commit_msg.py
+++ .github/actions/generate_commit_msg.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import openai
 
-openai.api_key = os.environ.get('CHATGPT_API_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 def get_code_diff():
     """Retrieve the code difference for the latest commit."""
