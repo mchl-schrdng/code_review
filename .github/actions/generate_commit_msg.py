893bc94bddb27be1809ef4fd329475326d5c78d2
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-21 13:33:50 +0200

    Update generate_commit_msg.py

--- .github/actions/generate_commit_msg.py
+++ .github/actions/generate_commit_msg.py
@@ -1,71 +1,30 @@
 import os
 import openai
-import git
+import subprocess
 
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+# Setup OpenAI SDK
+openai.api_key = os.environ["CHATGPT_API_KEY"]
 
-def get_diffs_for_file(repo, base_ref, head_ref, file_path):
-    """Gets the git diff for a specific file between two references."""
-    return repo.git.diff(base_ref, head_ref, file_path)
+def get_code_diff():
+    # Checkout the previous commit
+    subprocess.run(["git", "checkout", "HEAD^1"], check=True)
+    # Get the diff
+    result = subprocess.run(["git", "diff", "--name-only", "HEAD.."], capture_output=True, check=True, text=True)
+    return result.stdout
 
-def get_changed_files_diffs():
-    """Fetch the diffs of files that were changed in the latest commit."""
-    repo = git.Repo('./')  # Assumes the current directory is the repository
-
-    # Get the difference between the latest commit and its parent
-    base_ref = "HEAD^"
-    head_ref = "HEAD"
-    
-    changed_files = repo.git.diff(base_ref, head_ref, name_only=True).split('\n')
-
-    diffs = {}
-    for file_path in changed_files:
-        diffs[file_path] = get_diffs_for_file(repo, base_ref, head_ref, file_path)
-
-    # Combine all diffs into a single string
-    all_diffs = "\n".join(diffs.values())
-    return all_diffs
-
-def infer_emoji_from_message(message):
-    """Infer an appropriate emoji based on the content of the commit message."""
-    lower_message = message.lower()
-    if "fix" in lower_message:
-        return "🔧"  # Wrench for fixes
-    elif "update" in lower_message:
-        return "🔄"  # Arrows for updates
-    elif "delete" in lower_message or "remove" in lower_message:
-        return "❌"  # Cross mark for deletions
-    else:
-        return "✨"  # Sparkles for general enhancements
-
-def generate_commit_message(chunk):
-    """Generate a concise commit message based on the given code changes."""
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are an AI designed to generate concise and informative commit messages based on provided code changes."
-            },
-            {
-                "role": "user",
-                "content": f"Given the code changes, provide a concise and descriptive commit message:\n{chunk}"
-            }
-        ],
+def get_commit_message(diff):
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt=f"Explain the following code changes: {diff}",
+      max_tokens=150
     )
-    
-    # Extract the content of the assistant's message from the response
-    message = response['choices'][0]['message']['content']
-    emoji = infer_emoji_from_message(message)
-    
-    # Truncate the message if it's too long (e.g., limit to 50 characters)
-    max_length = 50
-    if len(message) > max_length:
-        message = message[:max_length - 3] + "..."
-    
-    return f"{emoji} {message}"
+    return response.choices[0].text.strip()
+
+def main():
+    diff = get_code_diff()
+    commit_message = get_commit_message(diff)
+    # Amend the latest commit with the generated message
+    subprocess.run(["git", "commit", "--amend", "-m", commit_message], check=True)
 
 if __name__ == "__main__":
-    diff = get_changed_files_diffs()
-    commit_message = generate_commit_message(diff)
-    print(f"::set-output name=message::{commit_message}")
+    main()
