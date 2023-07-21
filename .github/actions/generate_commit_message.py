50d4cb4a222600f238d387aecbf55995cface469
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-21 16:01:42 +0200

    : Converts commit messages to short formatted ones.
    What are you

--- .github/actions/generate_commit_message.py
+++ .github/actions/generate_commit_message.py
@@ -2,31 +2,45 @@ import os
 import openai
 import subprocess
 
-# Setup OpenAI SDK
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
 def get_code_diff():
+    """
+    Get the difference in code for the most recent commit.
+
+    Returns:
+        str: Description of the diff or 'init commit' for the first commit.
+    """
     commit_count = int(subprocess.getoutput("git rev-list --count HEAD"))
+    
     if commit_count == 1:
         return "init commit"
-    
-    # Get the diff for the latest commit
-    result = subprocess.getoutput("git diff HEAD^ --name-only")
-    return result
+
+    # Return the list of changed files for the latest commit
+    return subprocess.getoutput("git diff HEAD^ --name-only")
 
 def get_commit_message(diff):
+    """
+    Get the commit message using OpenAI for the provided code difference.
+
+    Args:
+        diff (str): Description of code difference.
+
+    Returns:
+        str: Generated commit message.
+    """
     if diff == "init commit":
         return diff
-    
+
     prompt = f"Describe these code changes in one sentence: {diff}"
     response = openai.Completion.create(
-      engine="davinci",
-      prompt=prompt,
-      max_tokens=15  # Limiting the length of the response to around 100 characters
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=15  # Limit to 15 tokens for brevity
     )
+    
     return response.choices[0].text.strip()
 
 def main():
+    """Main function to generate and amend the commit message."""
     diff = get_code_diff()
     commit_message = get_commit_message(diff)
     subprocess.run(["git", "commit", "--amend", "-m", commit_message], check=True)
